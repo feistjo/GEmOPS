ddd94e9b8b6b3fd2759d44695b12d693aa21d5f2
feistjo <joshua.feist@gmail.com>
2023-04-11 15:28:22 -0500

    Test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "galvo.h"
+
+Galvo x{19, 22, -255, 255, 8, 0};
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Ender an int to set galvo: ");
+    // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    String in = Serial.readStringUntil('\n');
+    x.SetPosition(std::atoi(in.c_str()));
+    // put your main code here, to run repeatedly:
 }
