c799c135db4e726e274527913b75b39278ca6583
ZoeSnead <107580325+ZoeSnead@users.noreply.github.com>
2023-06-01 19:27:26 -0500

    Update umm.cpp

--- src/umm.cpp
+++ src/umm.cpp
@@ -3,11 +3,75 @@
 #include <WiFi.h>
 #include <SPIFFS.h>
 
+const char *ssid = "GEmOPS";
+const char *password = "pleasebuy";
+
+AsyncWebServer server(HTTP_PORT); //TODO: check this? it's 80 in 
+AsyncWebSocket ws("/ws");
+
+void initWebServer() {
+    server.on("/", onRootRequest);
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+    server.begin();
+}
+
 void initSPIFFS() {
   if (!SPIFFS.begin()) {
     Serial.println("Cannot mount SPIFFS volume...");
-    // while(1) digitalWrite(LED_BUILTIN, millis() % 200 < 50 ? HIGH : LOW);
+    while(1) {
+    } 
   }
 }
 
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+  }
+  Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
+}
+
+void onRootRequest(AsyncWebServerRequest *request) {
+  request->send(SPIFFS, "/index.html", "text/html", false, processor);
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);  
+  server.addHandler(&ws);
+}
+
+void onEvent(AsyncWebSocket       *server,
+             AsyncWebSocketClient *client,
+             AwsEventType          type,
+             void                 *arg,
+             uint8_t              *data,
+             size_t                len) {
+
+    switch (type) {
+        case WS_EVT_CONNECT:
+            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+            break;
+        case WS_EVT_DISCONNECT:
+            Serial.printf("WebSocket client #%u disconnected\n", client->id());
+            break;
+        case WS_EVT_DATA:
+        case WS_EVT_PONG:
+        case WS_EVT_ERROR:
+            break;
+    }
+}
+
+void setup() {
+    Serial.begin(115200); delay(500);
+    WiFi.softAP(ssid, password);
+  
+    initSPIFFS();
+    initWiFi();
+    initWebSocket();
+    initWebServer();
+  
+}
 
